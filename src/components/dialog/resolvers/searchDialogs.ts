7f1f6dd6d0fe1dfd852c9f31bfd93aadfb0ce61f
Yury Saukou <yury_saukou@epam.com>
2022-01-29 16:17:08 +0300

    update queries for searching dialogs and users

--- src/components/dialog/resolvers/searchDialogs.ts
+++ src/components/dialog/resolvers/searchDialogs.ts
@@ -1,24 +1,45 @@
-import { Brackets } from 'typeorm';
+import { Any, Brackets, Raw } from 'typeorm';
 
 import { Dialog } from '../../../entity';
+
+const SAVED_MESSAGES = 'saved';
 
 export const searchDialogsResolver = async (_parent, { search }, { user }) => {
   try {
     // TODO: add validation; support filters
 
-    const searchValue = `%${search}%`;
+    const searchValue = `%${search.toLowerCase()}%`;
 
     const dialogs = await Dialog.createQueryBuilder('dialog')
       .leftJoinAndSelect('dialog.messages', 'message')
-      .leftJoinAndSelect('dialog.interlocutors', 'interlocutors')
+      .leftJoinAndSelect('dialog.interlocutors', 'interlocutorsSelect')
+      .leftJoin('dialog.interlocutors', 'interlocutors')
       .where(':userId = ANY (dialog.userIds)', { userId: user.userId })
       .andWhere(
         new Brackets((qb) => {
-          qb.where('interlocutors.username like :searchValue', { searchValue })
-            .orWhere('interlocutors.firstName like :searchValue', { searchValue })
-            .orWhere('interlocutors.lastName like :searchValue', { searchValue });
+          qb.where('"dialog"."userIds"[1] <> "dialog"."userIds"[2]')
+            .andWhere('interlocutors.username <> :username', { username: user.username })
+            .andWhere(
+              new Brackets((qb) => {
+                qb.where('LOWER(interlocutors.username) like :searchValue', { searchValue })
+                  .orWhere('LOWER(interlocutors.firstName) like :searchValue', { searchValue })
+                  .orWhere('LOWER(interlocutors.lastName) like :searchValue', { searchValue });
+              }))
         })
       )
+      .orWhere(new Brackets((qb) => {
+        qb.where('"dialog"."userIds"[1] = "dialog"."userIds"[2]')
+          .andWhere(
+            new Brackets((qb) => {
+              qb.where('LOWER(interlocutors.username) like :searchValue', { searchValue })
+                .orWhere('LOWER(interlocutors.firstName) like :searchValue', { searchValue })
+                .orWhere('LOWER(interlocutors.firstName) like :searchValue', { searchValue })
+                .orWhere(`'${SAVED_MESSAGES}' like :searchValue`, { searchValue });
+            }))
+      }))
+      .orderBy({
+        'message.updatedAt': 'DESC',
+      })
       .getMany();
 
     return dialogs;
--- src/components/user/resolvers/searchUsers.ts
+++ src/components/user/resolvers/searchUsers.ts
@@ -6,15 +6,15 @@ export const searchUsersResolver = async (_parent, { search }, { user }) => {
   try {
     // TODO: add validation; support filters
 
-    const searchValue = `%${search}%`;
+    const searchValue = `%${search.toLowerCase()}%`;
 
     const users = await User.createQueryBuilder('user')
       .where('user.id <> :userId', { userId: user.userId })
       .andWhere(
         new Brackets((qb) => {
-          qb.where('user.username like :searchValue', { searchValue })
-            .orWhere('user.firstName like :searchValue', { searchValue })
-            .orWhere('user.lastName like :searchValue', { searchValue });
+          qb.where('LOWER(user.username) like :searchValue', { searchValue })
+            .orWhere('LOWER(user.firstName) like :searchValue', { searchValue })
+            .orWhere('LOWER(user.lastName) like :searchValue', { searchValue });
         })
       )
       .getMany();
